b62b6830d96276ba0b081356bca900580bb507a1
Paras-79 <pbhai4632@gmail.com>
2026-02-19 11:09:55 +0530

    format main.cpp for consistency and readability

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,3 @@
-#include <Arduino.h>
 /**
  * @file main.cpp
  * @brief Embedded Force Measurement System using FSR
@@ -11,16 +10,15 @@
  */
 
 #include <Arduino.h>
-int value=0;
+
+int value = 0;
+
 void setup() {
-    // write your initialization code here
     Serial.begin(9600);
     pinMode(A0, INPUT);
-
 }
 
 void loop() {
-    // write your code here
     value = analogRead(A0);
     Serial.println(value);
     delay(500);
